e799a0ab7e1ef42d138dbad05490f2be98ff4116
Nathan De Aguiar Lima <nathan.lima@ciccr-ce.interno>
2022-11-17 13:24:03 -0300

    Models criados

---
+++ api/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const port = 3001;
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch(() => console.log('Erro ao conectar com mongodb'));
+
+
+
+
